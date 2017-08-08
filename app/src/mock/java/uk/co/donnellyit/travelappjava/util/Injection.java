75acf0c642072b309acd2fecc2ca7cdbbe6b1e6c
Chris Donnelly <chris.donnelly@keytree.co.uk>
2017-08-08 10:31:55 +0100

    missing Injection method

--- app/src/mock/java/uk/co/donnellyit/travelappjava/util/Injection.java
+++ app/src/mock/java/uk/co/donnellyit/travelappjava/util/Injection.java
@@ -2,6 +2,9 @@ package uk.co.donnellyit.travelappjava.util;
 
 import android.content.Context;
 
+import uk.co.donnellyit.travelappjava.ui.departures.DeparturesPresenter;
+import uk.co.donnellyit.travelappjava.ui.departures.LiveDeparturesInteractorImpl;
+import uk.co.donnellyit.travelappjava.ui.departures.StationsInteractorImpl;
 import uk.co.donnellyit.travelappjava.ui.neartrain.FakeLocationInteractor;
 import uk.co.donnellyit.travelappjava.ui.neartrain.FakeTrainNearInteractor;
 import uk.co.donnellyit.travelappjava.ui.neartrain.NearTrainPresenter;
@@ -13,5 +16,10 @@ import uk.co.donnellyit.travelappjava.ui.neartrain.NearTrainPresenter;
 public class Injection {
     public static NearTrainPresenter injectNearTrainPresenter(Context context) {
         return new NearTrainPresenter(context, new FakeLocationInteractor(), new FakeTrainNearInteractor());
+    }
+
+    public static DeparturesPresenter injectDeparturesPresenter(Context context) {
+        //TODO Replace interactors with fake ones
+        return new DeparturesPresenter(context, new LiveDeparturesInteractorImpl(), new StationsInteractorImpl(context));
     }
 }
